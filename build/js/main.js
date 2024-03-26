c95b9587ac26060108d7e9a91c78252cc9805118
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-26 23:09:54 +0100

    changes

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+const initApp=()=>{
+    const hamBurgerBtn=document.getElementById('hamburger-button');
+
+    const mobileMenu=document.getElementById('mobile-menu');
+
+
+    const toggleMenu=()=>{
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+
+    }
+
+    hamBurgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener ('click', toggleMenu)
+}
+document.addEventListener('DOMContentLoaded', initApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html","./build/js/*.js"],
   theme: {
     extend: {
       screens:{
@@ -8,6 +8,18 @@ module.exports = {
         'tallscreen':{"raw":'(min-aspect-ratio:13/20)'},
 
       },
+      keyframes:{
+
+        'open-menu':{
+          '0%':{transform:'scaleY(0)'},
+          '80%':{transform:'scaleY(1.2)'},
+          '100%':{transform:'scaleY(1)'},
+        },
+      },
+
+      animation:{
+        'open-menu':'open-menu 0.5s ease-in-out forwards'
+      }
     },
   },
   plugins: [],
