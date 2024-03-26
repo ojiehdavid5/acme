a643eadc5f408128c07f616d40fd05a70835ae41
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-26 22:27:06 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'widescreen':{"raw":'(min-aspect-ratio:9/2)'},
+        'tallscreen':{"raw":'(min-aspect-ratio:13/20)'},
+
+      },
+    },
   },
   plugins: [],
 }
